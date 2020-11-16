120e7bc7cdc84d5a29e082f7ca46b71e40ba197f
עוז פרץ <ozper@auth.ad.bgu.ac.il>
2020-11-16 15:45:30 +0200

    add TCploting

--- ClassTests.m
+++ ClassTests.m
@@ -3,6 +3,7 @@ exp1 = LaserExperiment(061120115023,'G:\.shortcut-targets-by-id\1EvfKa8tBidzgjdN
 exp1.LoadSensorData;
 
 exp1.SetTCParam(15, 5);
-exp1.plotTC;
-exp1.plotTCtimediff;
-exp1.plotTCProfileOverTime;
+%exp1.plotTC;
+%exp1.plotTCtimediff;
+%exp1.plotTCProfileOverTime;
+ exp1.plotTCPmovieOverTime ;
