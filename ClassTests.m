c5d25a907d1217f8f527e255f4fec0f4c5cc7d4b
BGU-USERS\idoshe <idoshe@post.bgu.ac.il>
2020-11-16 14:48:24 +0200

    add clock validition'

--- ClassTests.m
+++ ClassTests.m
@@ -1,8 +1,11 @@
 clc; clear; close all;
 exp1 = LaserExperiment(061120115023,'G:\.shortcut-targets-by-id\1EvfKa8tBidzgjdNARKNImsu2yD8J9I5j\laser defence plasan\exp\exp_system_first_run\061120115023');
 exp1.LoadSensorData;
-
+exp1.LoadVideoData;
 exp1.SetTCParam(15, 5);
 exp1.plotTC;
 exp1.plotTCtimediff;
 exp1.plotTCProfileOverTime;
+exp1.CheckClockValidSensor;
+exp1.CheckClockValidVideo;
+
