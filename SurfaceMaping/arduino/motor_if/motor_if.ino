e33cf75611675ef1721eba82d668e1aa6747fc89
Idosh <Idosh0@gmail.com>
2021-05-19 16:46:02 +0300

    IF now work

--- SurfaceMaping/arduino/motor_if/motor_if.ino
+++ SurfaceMaping/arduino/motor_if/motor_if.ino
@@ -1,28 +1,123 @@
+#include <Wire.h>
+#include <Adafruit_ADS1X15.h>
+
+//#define Dbg
+#define resolution 1
+#define dirPinw 4
+#define stepPinw 3
+#define dirPinL 2
+#define stepPinL 5
+#define stepsPerRevolution 200*9/360
+#define stepnum 3
+#define ADS1115SF 0.1875
+Adafruit_ADS1115 ads1115;
+
+
 void setup() {
   // put your setup code here, to run once:
-Serial.begin(9600);
+  Serial.begin(9600);
+  pinMode(dirPinL, OUTPUT);
+  pinMode(stepPinL, OUTPUT);
+  pinMode(dirPinw, OUTPUT);
+  pinMode(stepPinw, OUTPUT);
+  ads1115.begin(0x48);
+  ads1115.setGain(GAIN_TWOTHIRDS);
+
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  while (Serial.available()) {
+    String string_msg = Serial.readString();
+    int msg = string_msg.toInt();
+
+    int msg100 = msg / 100;
+    int temp   = msg - msg100 * 100;
+    int msg10  = temp / 10;
+    int msg1   = msg % 10;
+#ifdef Dbg
+    Serial.println(msg100);
+    Serial.println(msg10);
+    Serial.println(msg1);
+#endif
+
+    switch (msg100) {
+      case 1: Serial.println("Pong"); break;
+      case 2: case2_stepInput(msg10, msg1); break;
+      case 3: case3_getread(); break;
+      case 4: case4_stepAndRead(msg10, msg1); break;
+    }
+
+  }
 
 }
-void serialEvent() {
-  while (Serial.available()) {
-        String string_msg = Serial.readString();
-        int msg = string_msg.toInt();
-        switch(msg) {
-          case 1:
-          Serial.print("1111");
-          break;
-          case 2:
-          Serial.print("Pong");
-          break;
-          case 3:
-          Serial.print("3");
-          break;
-        }
-        
+//void serialEvent() {
+
+
+
+//}
+
+void case2_stepInput(int direc, int motor) {
+  String ip;
+  // int direc;
+  //Serial.print("C2S1");
+  //while(!Serial.available()){};
+  // ip = Serial.readString();
+  // ip.toInt() == 211 ? direc = HIGH : direc = LOW;
+  // Serial.print("C2S2");
+  // while(!Serial.available()){};
+  // ip = Serial.readString();
+  if (motor == 1) {
+    DoStep(dirPinw, stepPinw, direc);
   }
+  else if (motor == 0) {
+    DoStep(dirPinL, stepPinL, direc);
+  }
+}
+void case3_getread() {
+  int16_t results = ads1115.readADC_Differential_0_1();
+  Serial.println(results);
+}
+
+void case4_stepAndRead(int direc, int motor){
+  case2_stepInput(direc, motor);
+  case3_getread();
   
+}
+void start_scan(int Width, int Length) {
+
+  double  stepnum_w = Width / resolution ;
+  double stepnum_l = Length / resolution;
+  int direc;
+  for (int i = 0; i < stepnum_w; i++) {
+
+    //define direct value
+    i % 2 == 1 ? direc = HIGH : direc = LOW;
+    for (int j = 0; j < stepnum_l; j++) {
+      DoStep(dirPinw, stepPinw, direc);
+    }
+    direc = HIGH;
+    direc = LOW;
+    DoStep(dirPinL, stepPinL, direc);
+  }
+}
+
+void DoStep(int dirPin, int stepPin, int dir) {
+
+
+
+  // Set the spinning direction clockwise:
+
+  //rotation side, high for forword and low for backword (dir)
+  digitalWrite(dirPin, dir);
+  // Spin the stepper motor 1 revolution slowly:
+  for (int i = 0; i < stepsPerRevolution; i++) {
+    // These four lines result in 1 step:
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(800);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(800);
+  }
+  //delay between messure
+  delay(500);
 }
--- SurfaceMaping/arduino_pc_if/main.py
+++ SurfaceMaping/arduino_pc_if/main.py
@@ -1,5 +1,6 @@
 import serial as ser
 import time
+import csv
 
 
 class LaserScanner:
@@ -9,53 +10,98 @@ class LaserScanner:
         self.UserInput = 0
         self.Arduino = ser.Serial(port=self.Ch, baudrate=self.BaudRate, timeout=.1)
         self.MsgNum = 0
-        self.MsgBuffer = [[0, 0, 0]]
-        self.MsgLogger = []
+        self.dicbuf = {"X": [], "Y": [], "Z": []}
+        self.resbuf = [["X", "Y", "Z"]]
+        self.res = 0.05 * 5
 
     def showUI(self):
         _input = 0
-        while _input != '1' and _input != '2':
+        while _input != '1' and _input != '2' and _input != '3':
             _input = input("1 - Start scan \n"
-                           "2 - Exit\n")
-        self.UserInput = _input
+                           "2 - Pong\n"
+                           "3 - Exit\n")
+
+        userinput = _input
+        if userinput == 1:
+            W = int(input("Enter width [mm]:"))
+            L = int(input("Enter length [mm]:"))
+            self.StartScan(W, L)
 
     def SendStartCommand(self):
+        time.sleep(2)
         self.Arduino.write('1'.encode())
-
-    def SendPing(self):
-        self.Arduino.write('2'.encode())
         time.sleep(1)
         rv = self.Arduino.read(4)
         print(rv.decode('utf-8'))
-        self.Arduino.write('2'.encode())
+
+    def SendPing(self):
+        self.Arduino.write('100'.encode())
         time.sleep(1)
-        self.Arduino.flush()
-        rv = self.Arduino.read(4)
+        rv = self.Arduino.readline()
         print(rv.decode('utf-8'))
 
     def ReadDataMessage(self):
-        msg = [int(self.Arduino.read(1).decode('utf-8')),
-               int(self.Arduino.read(1).decode('utf-8')),
-               int(self.Arduino.read(1).decode('utf-8')),
-               int(self.Arduino.read(1).decode('utf-8'))]
-        print(msg)
-        self.MsgBuffer = msg
+        self.Arduino.write('300'.encode())
+        time.sleep(1)
+        rv = self.Arduino.readline()
+        print(rv.decode('utf-8'))
 
-    def SaveDataMsg(self):
-        self.MsgLogger.append(self.MsgBuffer)
+
+    def StartScan(self, W, L):
+        X = 0
+
+        while (X * self.res) < W:
+            direc = X % 2
+            Y = 0
+            while (Y * self.res) < L:
+                Z = self.StepAndRead(0, direc)
+                self.dicbuf['X'].append(X)
+                self.dicbuf['Y'].append(Y)
+                self.dicbuf['Z'].append(Z)
+                self.resbuf.append([X, Y, Z])
+                Y += 1
+            self.StepAndRead(1, 0)
+            X += 1
+
+    def DoStep(self, motor, direc):
+        msg = 200 + 10 * direc + motor
+        msg = str(msg)
+        self.Arduino.write(msg.encode())
+
+    def StepAndRead(self, motor, direc):
+        msg = 400 + 10 * direc + motor
+        msg = str(msg)
+        self.Arduino.write(msg.encode())
+        while not self.Arduino.in_waiting:
+            continue
+        rv = self.Arduino.readline()
+        print(rv.decode('utf-8'))
+        return int(rv.decode('utf-8'))
 
 
 if __name__ == '__main__':
     ls = LaserScanner('COM5', 9600)
-    ls.showUI()
-    ls.SendStartCommand()
     time.sleep(2)
-    ls.ReadDataMessage()
-    ls.SaveDataMsg()
-    ls.SendStartCommand()
-    time.sleep(2)
-    ls.ReadDataMessage()
-    ls.SaveDataMsg()
-    print(ls.MsgLogger)
-    ls.SendPing()
-
+    # ls.StepAndRead(1, 1)
+    # ls.StepAndRead(1, 0)
+    # ls.StepAndRead(1, 1)
+    # ls.StepAndRead(1, 0)
+    # ls.StepAndRead(1, 1)
+    # ls.StepAndRead(1, 0)
+    # ls.StepAndRead(1, 1)
+    # ls.StepAndRead(1, 0)
+    # ls.StepAndRead(1, 1)
+    # ls.StepAndRead(1, 0)
+    # ls.DoStep(1, 1)
+    # time.sleep(2)
+    # ls.DoStep(1, 0)
+    # time.sleep(2)
+    # ls.DoStep(0, 0)
+    # time.sleep(2)
+    # ls.DoStep(0, 1)
+    # ls.ReadDataMessage()
+    # time.sleep(2)
+    ls.StartScan(4, 4)
+    with open('innovators.csv', 'w', newline='') as file:
+        writer = csv.writer(file, delimiter=',')
+        writer.writerows(ls.resbuf)
